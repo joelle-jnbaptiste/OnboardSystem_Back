d8df0be4a4a0f14320ed657de02527e064b8b834
joelle-jnbaptiste <75635571+joelle-jnbaptiste@users.noreply.github.com>
2025-06-26 15:52:02 +0200

    store model in blob storage

--- main.py
+++ main.py
@@ -4,14 +4,29 @@ from PIL import Image
 import numpy as np
 import io
 import tensorflow as tf
+import requests
+import tempfile
+
+url = "https://modelstorages.blob.core.windows.net/models/saved_model_keras_larger.keras?sp=r&st=2025-06-26T13:41:30Z&se=2026-02-28T22:41:30Z&spr=https&sv=2024-11-04&sr=b&sig=MkehvHbN%2B1H94JdLgsRfZmR8LicBwBM9ban4g%2B9kPg8%3D"
+
+def load_model_from_blob(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    with tempfile.NamedTemporaryFile(suffix=".keras") as tmp:
+        tmp.write(response.content)
+        tmp.flush()
+        model = tf.keras.models.load_model(tmp.name)
+    return model
+
+model = load_model_from_blob(url)
 
 IMG_HEIGHT, IMG_WIDTH = 256, 512
 MEAN = [0.485, 0.456, 0.406]
 STD = [0.229, 0.224, 0.225]
 
 app = FastAPI()
-model = tf.keras.models.load_model(
-    "model/saved_model_keras_larger.keras")
+
+# model = tf.keras.models.load_model("model/saved_model_keras_larger.keras")
 
 
 def preprocess_image(file: UploadFile) -> np.ndarray:
