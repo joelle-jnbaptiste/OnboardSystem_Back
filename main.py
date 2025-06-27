c9769590a0e6529beef2ed93d0993cace8a7785d
joelle-jnbaptiste <75635571+joelle-jnbaptiste@users.noreply.github.com>
2025-06-27 09:20:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,34 @@ import io
 import tensorflow as tf
 import requests
 import tempfile
+import os
 
-url = "https://modelstorages.blob.core.windows.net/models/saved_model_keras_larger.keras?sp=r&st=2025-06-26T13:41:30Z&se=2026-02-28T22:41:30Z&spr=https&sv=2024-11-04&sr=b&sig=MkehvHbN%2B1H94JdLgsRfZmR8LicBwBM9ban4g%2B9kPg8%3D"
 
+PART_URLS = [
+    "sp=r&st=2025-06-27T07:15:13Z&se=2026-06-27T15:15:13Z&spr=https&sv=2024-11-04&sr=b&sig=3v9R9xVOQ2xjiW8MILUDNWiGjEmZlXeodaSjf6ZXzd0%3D",
+    "sp=r&st=2025-06-27T07:16:30Z&se=2026-06-27T15:16:30Z&spr=https&sv=2024-11-04&sr=b&sig=L0KT9xymqrSkC%2FJL03ZdrnTVb1uZy1EGUrObgxmjQSo%3D",
+    "sp=r&st=2025-06-27T07:16:48Z&se=2026-06-27T15:16:48Z&spr=https&sv=2024-11-04&sr=b&sig=Ar0wEq6meenllBVoEiUW2UZqiupmTlGP1Oly1QbUP%2BQ%3D",
+    "sp=r&st=2025-06-27T07:17:40Z&se=2026-06-27T15:17:40Z&spr=https&sv=2024-11-04&sr=b&sig=Slkb7p6foP%2FHl2qCZgWJFaXHIrZ10U0k1NEWUUGOVeU%3D",
+    "sp=r&st=2025-06-27T07:17:55Z&se=2025-06-27T15:17:55Z&spr=https&sv=2024-11-04&sr=b&sig=brisFh4qeCZDhMCxiMzbas7bv8SlvB6Q11iOgR2BaNk%3D",
+    "sp=r&st=2025-06-27T07:18:12Z&se=2026-06-27T15:18:12Z&spr=https&sv=2024-11-04&sr=b&sig=rMOT%2BjTo6lkbK3fb9mXfhslOkRZndUu418MT%2BLk%2Fqf4%3D",
+    "sp=r&st=2025-06-27T07:18:29Z&se=2026-06-27T15:18:29Z&spr=https&sv=2024-11-04&sr=b&sig=CGgZ1hT6xpQGBur6A7eDL0zNwHkMDUJESzXwzZseN1I%3D",
+    "sp=r&st=2025-06-27T07:18:46Z&se=2026-06-27T15:18:46Z&spr=https&sv=2024-11-04&sr=b&sig=%2BbqJvvTadVrzhHRDS1%2Fss6h9WNK%2ByVh48QYYuxkA78s%3D",
+    "sp=r&st=2025-06-27T07:19:07Z&se=2026-06-27T15:19:07Z&spr=https&sv=2024-11-04&sr=b&sig=N%2BZWEDKNHICU3KEU%2BNKwzf2l0oKT7wsxLwI1yFldtNs%3D",
+    "sp=r&st=2025-06-27T07:19:25Z&se=2026-06-27T15:19:25Z&spr=https&sv=2024-11-04&sr=b&sig=JB%2F%2FzEFLZQRqg7TG1IvGy2547WpxzQccL41w5OqHS00%3D",
+    "sp=r&st=2025-06-27T07:19:47Z&se=2026-06-27T15:19:47Z&spr=https&sv=2024-11-04&sr=b&sig=CWaPvNRSF06JDzuC56gme249NQUAATw86omtSufd%2BZI%3D"
+]
 
-def load_model_from_blob(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception(
-            f"Erreur de téléchargement du modèle : {response.status_code}")
-
-    # Créer un fichier temporaire qui ne sera pas supprimé automatiquement
+def download_and_merge_parts(urls):
     tmp = tempfile.NamedTemporaryFile(suffix=".keras", delete=False)
-    tmp.write(response.content)
-    tmp.close()  # Fermer pour libérer l'accès à TensorFlow
-
-    model = tf.keras.models.load_model(tmp.name)
-    return model
+    
+    for url in urls:
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise Exception(f"Erreur de téléchargement : {url}")
+        tmp.write(response.content)
+    
+    tmp.close()
+    return tmp.name
 
 
 model = None
@@ -76,8 +87,9 @@ def postprocess_prediction(pred: np.ndarray) -> bytes:
 
 @app.on_event("startup")
 async def load_model():
+    model_path = download_and_merge_parts(PART_URLS)
     global model
-    model = load_model_from_blob(url)
+    model = tf.keras.models.load_model(model_path)
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
